e6091462a166b33e68fbec45253f46b01740abee
jailkinglol-cyber <jailkinglol@gmail.com>
2025-09-14 21:16:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `Hi i miss your presence. This week i was really really sick and wanted to spam messages sayo.\n I wish i could have met you a bit earlier. Maybe i would have seen that cute little you instead of just looking at them in pictures.\n\nI miss you so much. I know you're doing well. Neshs is having a SN are you coming? we'd like to come if it's possible.\n\nI never wanted to be a bloke of the past but it seems inevitable in your world. Senior's life is moving fast. Hold on to it before you get taken away.\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `Hi i miss your presence. This week i was really really sick and wanted to spam messages sayo.\n I wish i could meet you. i just look into your pictures when i do miss you but sometimes i'll just stare at the ceiling aimlesly tjinking about u\n\nI miss you so much. I know you're doing well. Neshs is having a SN are you coming? Goodbye...\n\nI never wanted to be a bloke of the past but it seems inevitable in your world. Senior's life is moving fast. Hold on to it before you get taken away.\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -21,5 +21,6 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
 
 
